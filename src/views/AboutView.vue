e5baa96f83f9d35801899f32930577accd1d9c63
yamero <kraisorn.singsom@gmail.com>
2022-05-10 20:37:54 +0700

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ isAccept ? "Analytics is approve" : "Not approve" }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    isAccept: false,
+  }),
+  methods: {
+    cwcIsUserAccept() {
+      const response = window.cwcIsUserAccept('analytics')
+      this.isAccept = response 
+    },
+  },
+};
+</script>
