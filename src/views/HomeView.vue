6cd19560cb31df6f7439331275888cf4eff2c624
yamero <kraisorn.singsom@gmail.com>
2022-05-18 22:22:54 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,15 +30,21 @@ export default {
   },
 
   data: () => ({
-    url:'https://graph.facebook.com/v13.0/1752077728363529/feed?fields=full_picture%2Cmessage%2Cpermalink_url&access_token=',
-    token: "EAAHu6zfIL28BALJnG2PkEVTuZAF8EMSclPj3mDtoIiZAQb6xiqxgXOPmVsyLGdD0m9I961K7rZA66HzZBDZCg2lLuAZCZAEaS38TjNLoYZAplesJuEeZAZAatKk1wdwLgZAMHZCyS0FwZC1ButnWM3AmiS0GZBxtZAFlfEbzLqPdfsR8fZBnSyF1x5ytdkpjZAYkQ6jJsKCMUzEWvKArsVAZDZD",
+    url: "https://graph.facebook.com/v13.0/1752077728363529/feed?fields=full_picture%2Cmessage%2Cpermalink_url&access_token=",
+    token:
+      "EAAHu6zfIL28BALJnG2PkEVTuZAF8EMSclPj3mDtoIiZAQb6xiqxgXOPmVsyLGdD0m9I961K7rZA66HzZBDZCg2lLuAZCZAEaS38TjNLoYZAplesJuEeZAZAatKk1wdwLgZAMHZCyS0FwZC1ButnWM3AmiS0GZBxtZAFlfEbzLqPdfsR8fZBnSyF1x5ytdkpjZAYkQ6jJsKCMUzEWvKArsVAZDZD",
     dataList: [],
   }),
+  mounted() {
+    document.cookie = "name=oeschger; SameSite=None; Secure";
+  },
   methods: {
     async onGetFeeds() {
       const { token } = this.$route.query;
       console.log(token);
-      const response = await fetch(token ? `${this.url}${token}` : `${this.url}${this.token}`);
+      const response = await fetch(
+        token ? `${this.url}${token}` : `${this.url}${this.token}`
+      );
       const data = await response.json();
       this.dataList = data.data;
     },
